546654be10735f0ab1029fbfb9921352466c2872
rwilhel <rwilhel@gmail.com>
2017-09-12 23:54:23 -0600

    Adds Vuetify to the project for Vue Material styling

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import App from './App'
 import router from './router'
 import accounting from './accounting'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
