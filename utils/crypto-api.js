41af5b0d232e1fe591d3af22044b099459b68775
rwilhel <rwilhel@gmail.com>
2017-09-14 22:32:34 -0600

    Changes template literals to string concatenation

--- utils/crypto-api.js
+++ utils/crypto-api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = 'https://api.coinmarketcap.com'
 
 function getCoins() {
-  const url = `${BASE_URL}/v1/ticker/?limit=500`
+  const url = BASE_URL + "/v1/ticker/?limit=500"
 
   return axios.get(url).then(response =>
     response.data
